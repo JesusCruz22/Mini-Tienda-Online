c677b261fcb3052549787933c6db140f3c75619d
JesusCruz22 <59320374+JesusCruz22@users.noreply.github.com>
2022-08-26 14:44:16 -0400

    Connection with Firebase implemented.

---
+++ Recursos/js/Firebase.js
@@ -0,0 +1,18 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBBN7ntcPPmi8qrhYpSChqRVlkOhkbOjqo",
+    authDomain: "mini-tienda-online-41348.firebaseapp.com",
+    databaseURL: "https://mini-tienda-online-41348-default-rtdb.firebaseio.com",
+    projectId: "mini-tienda-online-41348",
+    storageBucket: "mini-tienda-online-41348.appspot.com",
+    messagingSenderId: "976815633406",
+    appId: "1:976815633406:web:2c73dec6a17aab440e06af"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
